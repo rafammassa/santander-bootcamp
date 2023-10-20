b8e6338c203d6e88505af468418e35170f2a17ea
Rafaella Massa <rafaella_massa@hotmail.com>
2023-10-20 16:28:35 -0300

    ResultadoEscolar com condicional ternária

---
+++ java/controle-fluxo/src/ResultadoEscolar2.java
@@ -0,0 +1,7 @@
+public class ResultadoEscolar2 {
+    public static void main(String[] args) {
+        int nota = 6;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
