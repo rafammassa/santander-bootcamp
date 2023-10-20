fa022143b684a5ad91da59db0806dccda5a921a3
Rafaella Massa <rafaella_massa@hotmail.com>
2023-10-20 16:31:29 -0300

    Adicionando "else if" à condicional ternária

--- java/controle-fluxo/src/ResultadoEscolar2.java
+++ java/controle-fluxo/src/ResultadoEscolar2.java
@@ -1,7 +1,7 @@
 public class ResultadoEscolar2 {
     public static void main(String[] args) {
-        int nota = 6;
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 ? "Em recuperação" : "Reprovado";
         System.out.println(resultado);
     }
 }
