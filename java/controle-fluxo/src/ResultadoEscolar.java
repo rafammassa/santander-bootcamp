5caa0b08311cb3c26b7155df344f1f265c1071a1
Rafaella Massa <rafaella_massa@hotmail.com>
2023-10-20 16:24:14 -0300

    Aprimorando ResultadoEscolar p condic. encadeada

--- java/controle-fluxo/src/ResultadoEscolar.java
+++ java/controle-fluxo/src/ResultadoEscolar.java
@@ -1,9 +1,11 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 5;
 
         if (nota >= 7) {
             System.out.println("Aprovado");
+        } else if (nota >= 5) {
+            System.out.println("Em recuperação");
         } else {
             System.out.println("Reprovado");
         }
